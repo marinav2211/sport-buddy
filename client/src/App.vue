d77280b724eaae6283b79ebe0187b0e29b06a6b6
marinav2211 <marinochka2288@gmail.com>
2023-11-04 22:40:56 +0100

    display items of users

---
+++ client/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div v-for="user in users">
+    <div class="user">
+      <p>{{ getFullDataOfUser(user) }}</p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    axios.get("http://localhost:3000/users")
+        .then(response => {
+          this.users = response.data
+        })
+  },
+methods: {
+  getFullDataOfUser(user) {
+    return `${user.name} - ${user['date-of-birth']} - ${user.sex}`
+  }
+}
+
+}
+</script>
+
+
+<style scoped>
+.user {
+  margin: 15px;
+  border: 2px solid teal;
+  padding: 10px;
+
+}
+</style>
